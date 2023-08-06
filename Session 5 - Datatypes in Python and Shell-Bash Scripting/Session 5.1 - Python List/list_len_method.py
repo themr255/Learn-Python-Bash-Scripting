9e3712c0c0611870cdcdd1dc8754c8d08de69b26
Mahesh Mahajan <115408179+themr255@users.noreply.github.com>
2023-08-06 18:24:22 +0530

    Added - list_len_method.py

---
+++ Session 5 - Datatypes in Python and Shell-Bash Scripting/Session 5.1 - Python List/list_len_method.py
@@ -0,0 +1,20 @@
+"""
+Method - len()
+
+Description: The len() method returns the number of items present in the list.
+"""
+
+# Creating a list for Punjabi Menu
+PUNJABI = ["Dal Makhni", "Rajma", "Paneer Tikka Masala", "Paneer Makhni", "Butter Roti", "Butter Naan", "Pudhina Paratha"]
+
+# Counting number of items present in PUNJABI list
+count_of_items = len(PUNJABI)
+
+# Finally, printing Count of Items in Punjabi Menu List
+print("Count of Items in Punjabi Menu: {}".format(count_of_items))
+
+"""
+                        Output - 
+---------------------------------------------------------------
+Count of Items in Punjabi Menu: 7
+"""
