d05ecc531685942e7db5456adc09270b50ea047f
Mahesh Mahajan <115408179+themr255@users.noreply.github.com>
2023-08-14 10:04:53 +0530

    Added - list_reverse_method.py

---
+++ Session 5 - Datatypes in Python and Shell-Bash Scripting/Session 5.1 - Python List/list_reverse_method.py
@@ -0,0 +1,25 @@
+"""
+Method - reverse()
+
+Description: The reverse() method reverses the elements of the list.
+"""
+
+# Creating a list for Punjabi Menu
+PUNJABI = ["Dal Makhni", "Rajma", "Paneer Tikka Masala", "Paneer Makhni", "Butter Roti", "Butter Naan", "Pudhina Paratha", "Plain Roti"]
+
+# Printing Punjabi Menu List
+print("Punjabi Menu: {}".format(PUNJABI))
+
+# Reversing the PUNJABI menu list.
+PUNJABI.reverse()
+
+# Finally, printing Punjabi Menu List again
+print("Punjabi Menu: {}".format(PUNJABI))
+
+"""
+                        Output - 
+---------------------------------------------------------------
+Punjabi Menu: ['Dal Makhni', 'Rajma', 'Paneer Tikka Masala', 'Paneer Makhni', 'Butter Roti', 'Butter Naan', 'Pudhina Paratha', 'Plain Roti']
+Punjabi Menu: ['Plain Roti', 'Pudhina Paratha', 'Butter Naan', 'Butter Roti', 'Paneer Makhni', 'Paneer Tikka Masala', 'Rajma', 'Dal Makhni']
+"""
+
