506222ed30980b4961936e2cce4712b62da50c6c
Mahesh Mahajan <115408179+themr255@users.noreply.github.com>
2023-08-17 11:34:32 +0530

    Added - list_clear_method.py

---
+++ Session 5 - Datatypes in Python and Shell-Bash Scripting/Session 5.1 - Python List/list_clear_method.py
@@ -0,0 +1,25 @@
+"""
+Method - clear()
+
+Description: The clear() method removes all items from the list.
+"""
+
+# Creating a list for Punjabi Menu
+PUNJABI = ['Butter Naan', 'Butter Roti', 'Dal Makhni', 'Paneer Makhni', 'Paneer Tikka Masala', 'Plain Roti', 'Pudhina Paratha', 'Rajma']
+
+# Printing Punjabi Menu List
+print("PUNJABI Menu: {}".format(PUNJABI))
+
+# Clearing PUNJABI menu list.
+PUNJABI.clear()
+
+# Finally, printing Punjabi Menu List again
+print("Punjabi Menu: {}".format(PUNJABI))
+
+"""
+                        Output - 
+---------------------------------------------------------------
+PUNJABI Menu: ['Butter Naan', 'Butter Roti', 'Dal Makhni', 'Paneer Makhni', 'Paneer Tikka Masala', 'Plain Roti', 'Pudhina Paratha', 'Rajma']
+Punjabi Menu: []
+"""
+
