0e7a0b878cb690d3dfbafbd7771fd816e00e586f
Mahesh Mahajan <115408179+themr255@users.noreply.github.com>
2023-09-01 21:15:57 +0530

    Added - dictionary_clear_method.py

---
+++ Session 5 - Datatypes in Python and Shell-Bash Scripting/Session 5.2 - Python Dictionary/dictionary_clear_method.py
@@ -0,0 +1,26 @@
+"""
+Method - clear()
+
+Description: Removes all items from the dictionary.
+"""
+
+# Creating a dictionary for Electronics Shop Inventory
+inventory = {"Laptop": 10, "Smartphone": 15, "Tablet": 5, "Headphones": 20}
+
+# Printing Electronics Shop Inventory
+print('Electronics Shop Inventory:', inventory)
+
+# Clear the Electronics Shop Inventory
+inventory.clear()
+
+# Printing Electronics Inventory after clearing it
+print('Electronics Shop Inventory after clearing it:', inventory)
+
+
+"""
+                        Output - 
+---------------------------------------------------------------
+Electronics Shop Inventory: {'Laptop': 10, 'Smartphone': 15, 'Tablet': 5, 'Headphones': 20}
+Electronics Shop Inventory after clearing it: {}
+"""
+
