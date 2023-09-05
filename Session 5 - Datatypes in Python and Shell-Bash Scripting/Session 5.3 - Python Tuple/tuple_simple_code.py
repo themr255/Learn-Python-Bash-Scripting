e529d5fc980fb226c632f94820afa8043bf2c0e1
Mahesh Mahajan <115408179+themr255@users.noreply.github.com>
2023-09-05 11:54:27 +0530

    Added - tuple_simple_code.py

---
+++ Session 5 - Datatypes in Python and Shell-Bash Scripting/Session 5.3 - Python Tuple/tuple_simple_code.py
@@ -0,0 +1,16 @@
+"""
+Description: Simple code to declare tuple and printing it.
+"""
+
+# Creating cartoon_characters tuple containing cartoon characters
+cartoon_characters = ('Turtle', 'Rabbit', 'Unicorn')
+
+# Print the tuple containing cartoon characters
+print("Cartoon Characters Tuple:", cartoon_characters)
+
+"""
+                        Output - 
+---------------------------------------------------------------
+Cartoon Characters Tuple: ('Turtle', 'Rabbit', 'Unicorn')
+"""
+
