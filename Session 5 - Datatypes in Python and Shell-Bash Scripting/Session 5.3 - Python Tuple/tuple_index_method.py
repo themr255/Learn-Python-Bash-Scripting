2517b936d6c8e04ed3368c2313e0fe23f2a1e8f0
Mahesh Mahajan <115408179+themr255@users.noreply.github.com>
2023-09-06 10:40:04 +0530

    Added - tuple_index_method.py

---
+++ Session 5 - Datatypes in Python and Shell-Bash Scripting/Session 5.3 - Python Tuple/tuple_index_method.py
@@ -0,0 +1,22 @@
+"""
+Method - index()
+
+Description: Returns the index of the specified element in the tuple.
+"""
+
+# Creating cartoon_characters tuple containing cartoon characters
+cartoon_characters = ('Turtle', 'Rabbit', 'Unicorn')
+print("Cartoon Characters Tuple:", cartoon_characters)
+
+# Getting index of 'Unicorn' in vowels
+index_of_unicorn = cartoon_characters.index('Unicorn')
+
+print("Index of Unicorn in Cartoon Characters Tuple:", index_of_unicorn)
+
+"""
+                        Output - 
+---------------------------------------------------------------
+Cartoon Characters Tuple: ('Turtle', 'Rabbit', 'Unicorn')
+Index of Unicorn in Cartoon Characters Tuple: 2
+"""
+
