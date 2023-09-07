4932618568517fbecd926dd86b027d722e2edd93
Mahesh Mahajan <115408179+themr255@users.noreply.github.com>
2023-09-07 14:49:51 +0530

    Added - tuple_count_method.py

---
+++ Session 5 - Datatypes in Python and Shell-Bash Scripting/Session 5.3 - Python Tuple/tuple_count_method.py
@@ -0,0 +1,23 @@
+"""
+Method - count()
+
+Description: Returns the number of times the specified element appears in the tuple.
+"""
+
+# Creating cartoon_characters tuple containing cartoon characters
+cartoon_characters = ('Turtle', 'Rabbit', 'Unicorn', 'Rabbit')
+print("Cartoon Characters Tuple:", cartoon_characters)
+
+# counts the number of times Rabbit appeared in the tuple
+count_of_rabbit = cartoon_characters.count('Rabbit')
+
+# Printing how many times 'Rabbit' appeared in cartoon_characters tuple
+print("Count of Rabbit in Cartoon Characters Tuple:", count_of_rabbit)
+
+"""
+                        Output - 
+---------------------------------------------------------------
+Cartoon Characters Tuple: ('Turtle', 'Rabbit', 'Unicorn', 'Rabbit')
+Count of Rabbit in Cartoon Characters Tuple: 2
+"""
+
