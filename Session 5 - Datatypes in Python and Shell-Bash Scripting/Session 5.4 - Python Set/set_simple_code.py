1a3ec30fa8cc891c8a17128eb7653c63839997cd
Mahesh Mahajan <115408179+themr255@users.noreply.github.com>
2023-09-08 23:32:38 +0530

    Added - set_simple_code.py

---
+++ Session 5 - Datatypes in Python and Shell-Bash Scripting/Session 5.4 - Python Set/set_simple_code.py
@@ -0,0 +1,15 @@
+"""
+Description: Simple code to declare set and printing it.
+"""
+
+# Set containing Sri Lanka Travel Destinations
+travel_destinations = {"Colombo", "Kandy", "Galle", "Sigiriya", "Ella", "Nuwara Eliya", "Mirissa", "Polonnaruwa"}
+
+# Print the set containing Sri Lanka travel destinations
+print("Popular travel destinations in Sri Lanka: {}".format(travel_destinations))
+
+"""
+                        Output - 
+---------------------------------------------------------------
+Popular travel destinations in Sri Lanka: {'Nuwara Eliya', 'Ella', 'Mirissa', 'Polonnaruwa', 'Sigiriya', 'Colombo', 'Kandy', 'Galle'}
+"""
