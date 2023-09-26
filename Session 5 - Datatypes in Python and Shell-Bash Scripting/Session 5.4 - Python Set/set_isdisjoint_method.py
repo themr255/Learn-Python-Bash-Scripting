f1a6f99ca949bcd51855cd54526b67fd4c25a706
Mahesh Mahajan <115408179+themr255@users.noreply.github.com>
2023-09-26 19:05:30 +0530

    Added - set_isdisjoint_method.py

---
+++ Session 5 - Datatypes in Python and Shell-Bash Scripting/Session 5.4 - Python Set/set_isdisjoint_method.py
@@ -0,0 +1,36 @@
+"""
+Method - isdisjoint()
+
+Description: Returns True if two sets don't have any common items between them, i.e. they are disjoint. Else the
+             returns False. """
+
+# Define sets representing different finish options for Asian Paints color products
+smooth_finish_colors = {"Satin Blue", "Antique White", "Classic Ivory", "Silk White"}
+ultra_sheen_finish_colors = {"Satin Blue", "Glossy Black", "Metallic Silver"}
+
+# Print the set of smooth_finish_colors
+print("Smooth Finish Colors:", smooth_finish_colors)
+
+# Print the set of ultra_sheen_finish_colors
+print("Ultra Sheen Finish Colors:", ultra_sheen_finish_colors)
+
+# Check if two sets - smooth_finish_colors and ultra_sheen_finish_colors have no common elements
+are_disjoint = smooth_finish_colors.isdisjoint(ultra_sheen_finish_colors)
+
+# Here smooth_finish_colors and ultra_sheen_finish_colors sets have one common color - Satin Blue
+# So these sets are not Disjoint
+
+# Print the all the colors present in both color type
+print("\nAre Sets Disjoint:", are_disjoint)
+
+
+"""
+                        Output - 
+---------------------------------------------------------------
+Smooth Finish Colors: {'Satin Blue', 'Antique White', 'Silk White', 'Classic Ivory'}
+Ultra Sheen Finish Colors: {'Satin Blue', 'Glossy Black', 'Metallic Silver'}
+
+Colors that are present in either of the sets but not in both:
+{'Classic Ivory', 'Antique White', 'Glossy Black', 'Silk White', 'Metallic Silver'}
+
+"""
